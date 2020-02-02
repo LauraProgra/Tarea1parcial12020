4d53cc0f881dd318872911814607e23259211354
Unknown <suazo7926@gmail.com>
2020-02-01 22:26:02 -0600

    Tarea1

---
+++ PA12020-1Ina suazo/src/pa12020/pkg1ina/suazo/PA120201InaSuazo.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package pa12020.pkg1ina.suazo;
+
+/**
+ *
+ * @author inalaurasuazo
+ */
+public class PA120201InaSuazo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
